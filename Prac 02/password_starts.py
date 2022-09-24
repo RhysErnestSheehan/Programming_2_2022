95fa048e7a8384c82f15faf59f55982869a37c45
Rhys <rhys.e.sheehan@protonmail.com>
2022-09-24 22:09:02 +1000

    Refactor password check program to use functions

---
+++ Prac 02/password_starts.py
@@ -0,0 +1,22 @@
+"""Rhys Sheehan CP1404 Prac 02 Password Starts"""
+
+
+def main():
+    password = get_password()
+    print_asterisks(password)
+
+
+def get_password():
+    password_minimum_length = 8
+    password = input("What is your new password?: ")
+    while len(password) < password_minimum_length:
+        print("Invalid password, please try again.")
+        password = input("What is your new password?: ")
+    return password
+
+
+def print_asterisks(password):
+    print(len(password) * "**")
+
+
+main()
