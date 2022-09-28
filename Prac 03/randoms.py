9f42eb1769e289ce12d0bca117ab830a334f7416
Rhys <rhys.e.sheehan@protonmail.com>
2022-09-28 10:01:28 +1000

    Answered Questions

---
+++ Prac 03/randoms.py
@@ -0,0 +1,15 @@
+# Question 1
+# I saw it print a random number between 5 and 20.
+# The smallest number I could of seen was 5 and the largest was 20.
+
+# Question 2
+# It prints random number from 3 to 10 in intervals of 2.
+# The smallest number you could see is 3 and the highest is 10.
+# You could not see a 4.
+
+# Question 3
+# A random number between 2.5 and 5.5 was printed.
+# The smallest number you could have seen was 2.5 and the largest was 5.5.
+
+import random
+print(random.randint(1, 100))
