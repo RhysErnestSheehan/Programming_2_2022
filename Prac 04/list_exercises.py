185e7cb5bd413ca816d6a0d2f70f1c9539a53265
Rhys <rhys.e.sheehan@protonmail.com>
2022-10-12 20:52:33 +1000

    Add usernames program

--- Prac 04/list_exercises.py
+++ Prac 04/list_exercises.py
@@ -9,3 +9,13 @@ print(f"The first number is", numbers[0])
 print(f"The last number is", numbers[4])
 print(f"The smallest number is", min(numbers))
 print(f"The largest number is", max(numbers))
+
+
+usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45', 'BaseInterpreterInterface', 'BaseStdIn',
+             'Command', 'ExecState', 'InteractiveConsole', 'InterpreterInterface', 'StartServer', 'bob']
+
+username = input("What is your username?: ")
+if username in usernames:
+    print("Access granted")
+else:
+    print("Access denied")
